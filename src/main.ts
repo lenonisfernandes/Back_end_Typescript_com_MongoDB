2ae26b942198cf46650d40263f935ef28af4d358
renan infnet <engrtorres@outlook.com>
2025-09-02 18:58:40 -0300

    feat: RouteNotFoundMiddleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import routes from './Api/routes';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import Logger from './Infra/Logger';
 import { basicAuthMiddleware } from './basicAuth';
 import ErrorHandler from './Infra/ErrorHandler';
 import setupSwagger from './Api/config/Swagger';
+import NotFoundException from './Api/Exceptions/NotFoundExpection';
 
 const app = express();
 const port = 3000;
@@ -20,6 +21,11 @@ app.get('/', (req: Request, res: Response) => {
     res.json('Bem vindo a primeira rota!!!');
 });
 
+const routeNotFoundMiddleware =
+    (req: Request, res: Response, next: NextFunction) =>
+        next(new NotFoundException('Rota não disponível'));
+
+app.use(routeNotFoundMiddleware);
 app.use(ErrorHandler.init());
 
 app.listen(port, () => {
